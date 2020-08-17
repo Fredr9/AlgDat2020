6124f88cd6ca7022f65a544102fdecef257c8770
Fredr9 <fredrik_mv_90@hotmail.com>
2020-08-17 12:11:41 +0200

    Ukesoppgave 1

---
+++ src/Ukeoppgaver1.java
@@ -0,0 +1,17 @@
+public class Ukeoppgaver1 {
+    public static void main(String[] args) {
+
+        int[] a = {8, 4, 17, 19, 6, 20, 1, 11, 15, 3, 18, 9, 2, 7, 18};
+
+        int min_index = min(a);
+
+        System.out.println(min_index);
+    }
+        static int min ( int[] a){
+
+            int min_value = a[0];
+            int min_index = 0;
+            return min_index;
+        }
+    }
+
